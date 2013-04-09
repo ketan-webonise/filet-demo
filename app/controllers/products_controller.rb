5e4aec5deea8598fd77703ca2baf015a0629c51b
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-04-09 18:59:58 +0530

    models added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,16 @@
 class ProductsController < ApplicationController
   def index
+    @products=Product.all
+    #@products.each do |p|
+    #@product=p
+    #  logger.info"##########################{@product.inspect}"
+    #end
+    respond_to do |format|
+      format.html{}
+      format.js{}
+      format.json{ render :json =>  @products.as_json }
+      logger.info"##################{@products.inspect}"
 
+    end
   end
 end
